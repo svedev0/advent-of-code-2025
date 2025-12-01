cecb9d6b5f01d864642c45155ffb1d6c8c16a514
svedev0 <emanuel@kulich.me>
2025-12-01 19:54:02 +0100

    Day 01

---
+++ Day01/Day01.cs
@@ -0,0 +1,98 @@
+﻿namespace AOC2025;
+
+internal class Day01
+{
+	public static void SolvePart1()
+	{
+		List<string> input = [.. File.ReadAllLines("Day01\\input.txt")];
+
+		int dial = 50;
+		int zeros = 0;
+
+		foreach (string line in input)
+		{
+			char dir = line[0];
+			int dist = int.Parse(line[1..]);
+
+			if (dir == 'R')
+			{
+				for (int i = 0; i < dist; i++)
+				{
+					dial++;
+					if (dial > 99)
+					{
+						dial = 0;
+					}
+				}
+				if (dial == 0)
+				{
+					zeros++;
+				}
+			}
+			else if (dir == 'L')
+			{
+				for (int i = 0; i < dist; i++)
+				{
+					dial--;
+					if (dial < 0)
+					{
+						dial = 99;
+					}
+				}
+				if (dial == 0)
+				{
+					zeros++;
+				}
+			}
+		}
+
+		Console.WriteLine($"[Part 1] Answer: {zeros}");
+	}
+
+	public static void SolvePart2()
+	{
+		List<string> input = [.. File.ReadAllLines("Day01\\input.txt")];
+
+		int dial = 50;
+		int zeros = 0;
+
+		foreach (string line in input)
+		{
+			char dir = line[0];
+			int dist = int.Parse(line[1..]);
+
+			if (dir == 'R')
+			{
+				for (int i = 0; i < dist; i++)
+				{
+					dial++;
+					if (dial > 99)
+					{
+						dial = 0;
+					}
+					if (dial == 0)
+					{
+						zeros++;
+					}
+				}
+			}
+			else if (dir == 'L')
+			{
+				for (int i = 0; i < dist; i++)
+				{
+					dial--;
+					if (dial < 0)
+					{
+						dial = 99;
+					}
+					if (dial == 0)
+					{
+						zeros++;
+					}
+				}
+			}
+		}
+
+		Console.WriteLine($"[Part 2] Answer: {zeros}");
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+
+namespace AOC2025;
+
+internal class Program
+{
+	public static void Main(string[] args)
+	{
+		int day = ValidateArgs(args);
+
+		Console.WriteLine($"""
+			===== Advent of Code 2025 =====
+
+			Day {day:D2}:
+			
+			""");
+
+		long startTime = Stopwatch.GetTimestamp();
+
+		switch (day)
+		{
+			case 1:
+				Day01.SolvePart1();
+				Day01.SolvePart2();
+				break;
+			default:
+				Console.WriteLine("Day not found");
+				break;
+		}
+
+		TimeSpan elapsed = Stopwatch.GetElapsedTime(startTime);
+		int elapsedMs = (int)Math.Floor(elapsed.TotalMilliseconds);
+		Console.WriteLine($"The program finished in {elapsedMs} ms");
+	}
+
+	private static int ValidateArgs(string[] args)
+	{
+		if (args.Length != 1 || !args[0].StartsWith("-day="))
+		{
+			throw new Exception("Missing argument 'day'. Example: '-day=1'");
+		}
+
+		string dayStr = args[0][5..].Trim();
+		if (!int.TryParse(dayStr, out int day) || day is < 1 or > 12)
+		{
+			throw new Exception("Invalid value for 'day'");
+		}
+
+		return day;
+	}
+}
